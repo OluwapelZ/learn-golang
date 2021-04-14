3cba28c06d7ee455f4701838b3ff1a78864a0ea0
Oluwapelz <olaoyeoluwapelumi@gmail.com>
2021-04-14 08:44:45 +0100

    deployment prep

--- go.mod
+++ go.mod
@@ -2,4 +2,32 @@ module github.com/OluwapelZ/learn-golang.git
 
 go 1.16
 
-require github.com/go-chi/chi v1.5.4
+require (
+	github.com/astaxie/beego v1.12.3
+	github.com/beego/bee/v2 v2.0.2 // indirect
+	github.com/beego/beego/v2 v2.0.1 // indirect
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-delve/delve v1.6.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/lib/pq v1.10.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pelletier/go-toml v1.9.0 // indirect
+	github.com/peterh/liner v1.2.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	go.starlark.net v0.0.0-20210406145628-7a1108eaa012 // indirect
+	golang.org/x/arch v0.0.0-20210405154355-08b684f594a5 // indirect
+	golang.org/x/sys v0.0.0-20210412220455-f1c623a9e750 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,77 @@
+// package main
+
+// // import "fmt"
+// import (
+// 	_"github.com/go-chi/chi"
+// 	"fmt"
+// )
+
+// func main() {
+// 	fmt.Println("Hello World!")
+// }
+
 package main
 
-// import "fmt"
 import (
-	_"github.com/go-chi/chi"
 	"fmt"
+    "strconv"
+    "github.com/astaxie/beego"
 )
 
+
 func main() {
-	fmt.Println("Hello World!")
+    /* This would match routes like the following:
+       /sum/3/5
+       /product/6/23
+       ...
+    */
+	beego.Router("/health", &healthCheckController{})
+    beego.Router("/:operation/:num1:int/:num2:int", &mainController{})
+    beego.Run()
+}
+
+type healthCheckController struct {
+	beego.Controller
+}
+
+type mainController struct {
+    beego.Controller
+}
+
+func (c *healthCheckController) Get() {
+	fmt.Print("Hello World!");
+	c.Data["result"] = "Hello World!";
+}
+
+
+func (c *mainController) Get() {
+
+    //Obtain the values of the route parameters defined in the route above    
+    operation := c.Ctx.Input.Param(":operation")
+    num1, _ := strconv.Atoi(c.Ctx.Input.Param(":num1"))
+    num2, _ := strconv.Atoi(c.Ctx.Input.Param(":num2"))
+
+    //Set the values for use in the template
+    c.Data["operation"] = operation
+    c.Data["num1"] = num1
+    c.Data["num2"] = num2
+    c.TplName = "result.html"
+
+    // Perform the calculation depending on the 'operation' route parameter
+    switch operation {
+    case "sum":
+        c.Data["result"] = add(num1, num2)
+    case "product":
+        c.Data["result"] = multiply(num1, num2)
+    default:
+        c.TplName = "invalid-route.html"
+    }
+}
+
+func add(n1, n2 int) int {
+    return n1 + n2
+}
+
+func multiply(n1, n2 int) int {
+    return n1 * n2
 }
